cb3e33a1418fbc21279e8c710d63ad70885067a9
Callum Wilson <calwilson@live.co.uk>
2021-07-02 23:05:20 +0100

    Add script to block nvidia packages from being automatically upgrade

---
+++ xubuntu/block-nvidia-upgrades.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+apt-mark hold \
+libnvidia-cfg1-460 \
+libnvidia-common-460 \
+libnvidia-compute-460 \
+libnvidia-compute-460 \
+libnvidia-decode-460 \
+libnvidia-decode-460 \
+libnvidia-encode-460 \
+libnvidia-encode-460 \
+libnvidia-extra-460 \
+libnvidia-fbc1-460 \
+libnvidia-fbc1-460 \
+libnvidia-gl-460 \
+libnvidia-gl-460 \
+libnvidia-ifr1-460 \
+libnvidia-ifr1-460 \
+nvidia-compute-utils-460 \
+nvidia-dkms-460 \
+nvidia-driver-460 \
+nvidia-kernel-common-460 \
+nvidia-kernel-source-460 \
+nvidia-prime \
+nvidia-settings \
+nvidia-utils-460 \
+xserver-xorg-video-nvidia-460 \
